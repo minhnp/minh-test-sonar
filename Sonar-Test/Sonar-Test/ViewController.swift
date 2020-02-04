571cd95b8d288b78f627d5298957bb2d057ff675
minhnp <minh13t1@gmail.com>
2020-02-04 15:24:20 +0700

    init-code

---
+++ Sonar-Test/Sonar-Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sonar-Test
+//
+//  Created by Admin on 2/4/20.
+//  Copyright © 2020 NPM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
